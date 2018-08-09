1ac9f3bfcf7dccd6acc83ee48a5449455553a249
Peter Datahider <peter@datahider.ru>
2018-08-09 21:09:46 +0300

    Орфография

--- lhWebhook/abstract/lhAbstractBotWebhook.php
+++ lhWebhook/abstract/lhAbstractBotWebhook.php
@@ -138,12 +138,12 @@ abstract class lhAbstractBotWebhook implements lhWebhookInterface{
             } else {
                 $answer = [ 'text' => 'У вас нет прав на установку администратора этого бота'];
             }
-            $this->session-set('bot_command', '');
+            $this->session->set('bot_command', '');
         } elseif(!$yes) {
-            $this->session-set('bot_command', '/setadmin');
+            $this->session->set('bot_command', '/setadmin');
             $answer = [ 'text' => 'Предоставить права администратора', 'hints' => ['Да', 'Нет']];
         } else {
-            $this->session-set('bot_command', '');
+            $this->session->set('bot_command', '');
             $answer = [ 'text' => 'У вас нет прав на установку администратора этого бота'];
         }
         return $answer;
